64d198ac8b490ac432b821bcad811b084a821f3f
hemanth <saihemanth9490@gmail.com>
2021-08-31 12:29:00 +0530

    Small changes in detail page view

--- src/Components/DetailPage/index.js
+++ src/Components/DetailPage/index.js
@@ -36,10 +36,27 @@ class DetailPage extends Component {
 
     return (
       <>
+        <Link to="/">
+          <button className="btn mt-3 d-md-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              className="bi bi-arrow-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+              />
+            </svg>
+          </button>
+        </Link>
         {ifTrue ? (
           <div className="container d-flex flex-row mt-5 align-items-start">
             <Link to="/">
-              <button className="btn mt-3">
+              <button className="btn mt-3 d-none d-md-block">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="50"
